caf0bbdfbf89b91d6cc64aeb7e423dea9eb84dbe
Alex <ango911s@gmail.com>
2024-05-24 23:50:58 -0700

    error in components

--- components/Map/LazyMap.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-
-const LazyMap = dynamic(() => import("@/components/Map/Map"), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
-
-export default LazyMap;
