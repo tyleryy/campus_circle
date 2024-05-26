095dcbdadacc6a9f112ef955ad21206b8310a00a
Alex <ango911s@gmail.com>
2024-05-26 00:03:34 -0700

    landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Heart } from "lucide-react";
 import { ModeToggle } from "@/components/dark-mode-toggle";
 
 const Footer: React.FC = () => {
   return (
     <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-between items-center text-center text-xs">
-      <p className="relative justify-center">Made with ❤️ by CampusCompass</p>
+      <p className="relative justify-center">
+        Made with ❤️ by <a className="text-blue-400">CampusCompass</a>
+      </p>
       <div className="relative justify-end">
         <ModeToggle />
       </div>
